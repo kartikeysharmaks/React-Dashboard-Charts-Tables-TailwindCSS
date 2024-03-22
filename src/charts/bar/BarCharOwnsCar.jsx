282038a298245ca194e2e0264b71c59467bb1fe0
kartikeysharmaks <kartikeysharma1616@gmail.com>
2024-03-23 01:48:26 +0530

    fixes

--- src/charts/bar/BarCharOwnsCar.jsx
+++ src/charts/bar/BarCharOwnsCar.jsx
@@ -1,4 +1,3 @@
-// GenderCarOwnershipBarChart.js
 import React from "react";
 import { Chart } from "react-google-charts";
 import Loader from "../../components/Loader";
@@ -8,7 +7,6 @@ const BarChartOwnsCar = ({ data }) => {
     return <Loader />;
   }
 
-  // Count males and females who own and do not own cars
   let malesOwnCar = 0;
   let malesDoNotOwnCar = 0;
   let femalesOwnCar = 0;
@@ -44,7 +42,6 @@ const BarChartOwnsCar = ({ data }) => {
     }
   });
 
-  // Prepare data for Google Charts
   const chartData = [
     ["Gender", "Total Count", "Owns Car", "Does Not Own Car"],
     ["Males", maleCount, malesOwnCar, malesDoNotOwnCar],
@@ -53,24 +50,25 @@ const BarChartOwnsCar = ({ data }) => {
   ];
 
   return (
-    <div className='m-4 md:m-8'>
-      <h2 className='text-[20px] md:text-[28px] py-2 font-bold'>Gender Car Ownership</h2>
+    <div className="m-4 md:m-8">
+      <h2 className="text-[20px] md:text-[28px] py-2 font-bold">
+        Gender Car Ownership
+      </h2>
       <Chart
         width={"70vw"}
         height={"80vh"}
         chartType="BarChart"
-    
         data={chartData}
         options={{
           title: "Gender Car Ownership",
           animation: {
-            startup: true, // Enable animation when the chart is first drawn
-            duration: 1000, // Animation duration in milliseconds
-            easing: 'out', // Animation easing function
+            startup: true,
+            duration: 1000,
+            easing: "out",
           },
           hAxis: { title: "Count", minValue: 0 },
-          vAxis: { title: "Gender", gridlines: { count: 0 }, },
-          legend: { position: 'bottom' },
+          vAxis: { title: "Gender", gridlines: { count: 0 } },
+          legend: { position: "bottom" },
         }}
       />
     </div>
